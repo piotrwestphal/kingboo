a5229d167fc716f89df8f69fb42b12e04ccb40f0
piotrwestphal <piotr.westphal@gmail.com>
2020-05-09 22:54:07 +0200

    fix date formatting

--- backend/libs/util/src/time.helper.ts
+++ backend/libs/util/src/time.helper.ts
@@ -1,9 +1,9 @@
 export class TimeHelper {
   public static getDiffTimeInSeconds = (refTimeMs: number): number => Math.round((Date.now() - refTimeMs) / 1000);
   public static getFormattedDate = (date: Date): string => date.toISOString().replace(/[^0-9]/g, '');
-  public static getFormattedShortDate = (date: Date): string => `${TimeHelper.twoDigitFormat(date.getFullYear())}-` +
+  public static getFormattedShortDate = (date: Date): string => `${date.getFullYear()}-` +
     `${TimeHelper.twoDigitFormat(date.getMonth() + 1)}-${TimeHelper.twoDigitFormat(date.getDate())}`;
-  public static getFormattedDirectoryShortDate = (date: Date): string => `${TimeHelper.twoDigitFormat(date.getFullYear())}` +
+  public static getFormattedDirectoryShortDate = (date: Date): string => `${date.getFullYear()}` +
     `${TimeHelper.twoDigitFormat(date.getMonth() + 1)}${TimeHelper.twoDigitFormat(date.getDate())}`;
   public static addDays(date: Date, daysToAdd: number): Date {
     const result = new Date(date);
