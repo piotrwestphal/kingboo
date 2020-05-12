a9eba6db2a377d20dbb4e0cb099efdb74831f2b9
piotrwestphal <piotr.westphal@gmail.com>
2020-05-13 00:35:17 +0200

    prepare for deploy to westus

--- cloud/opc/opc-init-vm.sh
+++ cloud/opc/opc-init-vm.sh
@@ -6,7 +6,7 @@ yum-config-manager \
 
 yum install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
-curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 yum install -y git
 
