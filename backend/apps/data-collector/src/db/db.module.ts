9034127eb1f449c679fee7330a4d2cfcdefd1f15
piotrwestphal <piotr.westphal@gmail.com>
2020-07-14 14:38:34 +0200

    add a missing scrap activity repository as a dependency

--- backend/apps/data-collector/src/db/db.module.ts
+++ backend/apps/data-collector/src/db/db.module.ts
@@ -6,6 +6,12 @@ import { FirestoreRawSearchResultRepository } from './firestore-raw-search-resul
 import { RawSearchResultMapper } from './raw-search-result/raw-search-result.mapper';
 import { MongoModule } from '@kb/mongo';
 import { ScrapActivitySchema, ScrapActivitySchemaKey } from './scrap-activity/scrap-activity.schema';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { ScrapActivityRepository } from '../core/abstract/scrap-activity.repository';
+import { ScrapActivityDocument } from './scrap-activity/scrap-activity.document';
+import { MongoScrapActivityRepository } from './mongo-scrap-activity.repository';
+import { ScrapActivityDocumentMapper } from './scrap-activity/scrap-activity-document.mapper';
 
 @Module({
   imports: [
@@ -23,9 +29,18 @@ import { ScrapActivitySchema, ScrapActivitySchemaKey } from './scrap-activity/sc
       },
       inject: [FirestoreClient],
     },
+    {
+      provide: ScrapActivityRepository,
+      useFactory: (model: Model<ScrapActivityDocument>) => {
+        const mapper = new ScrapActivityDocumentMapper();
+        return new MongoScrapActivityRepository(mapper, model);
+      },
+      inject: [getModelToken(ScrapActivitySchemaKey)],
+    },
   ],
   exports: [
     RawSearchResultRepository,
+    ScrapActivityRepository,
   ],
 })
 export class DbModule {
