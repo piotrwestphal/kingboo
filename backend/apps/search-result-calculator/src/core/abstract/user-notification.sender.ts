20275a4d72c1d7efc96c6fb30e0ffd8e261db0a3
piotrwestphal <piotr.westphal@gmail.com>
2020-07-27 23:13:12 +0200

    notify about hotels processing finish

--- backend/apps/search-result-calculator/src/core/abstract/user-notification.sender.ts
+++ backend/apps/search-result-calculator/src/core/abstract/user-notification.sender.ts
@@ -1,2 +1,5 @@
 export abstract class UserNotificationSender {
+  abstract notifyAboutHotelsProcessingFinished(searchId: string,
+                                               hotelIds: string[],
+                                               messageProcessingTime: number): void;
 }
