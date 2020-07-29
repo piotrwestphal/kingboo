bd8cd560ff55a850416e445636686b60284d2bc3
piotrwestphal <piotr.westphal@gmail.com>
2020-07-29 07:28:40 +0200

    remove useless method

--- backend/apps/data-collector/src/core/abstract/scrap-activity.repository.ts
+++ backend/apps/data-collector/src/core/abstract/scrap-activity.repository.ts
@@ -2,8 +2,6 @@ import { ScrapActivity } from "../model/ScrapActivity";
 
 export abstract class ScrapActivityRepository {
 
-  abstract createOrUpdate(searchId: string, scrapActivity: ScrapActivity): Promise<ScrapActivity>;
-
   abstract update(searchId: string, scrapActivity: ScrapActivity): Promise<ScrapActivity>;
 
   abstract findLastUpdatedGivenDaysAgo(now: Date, days: number): Promise<string[]>;
