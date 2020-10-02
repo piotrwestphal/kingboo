c8aac572a3e3d8e11ebaa9a7a3b4103b6fcd6b58
piotrwestphal <piotr.westphal@gmail.com>
2020-10-02 19:07:03 +0200

    add third column to top hotels data display

--- webapp/src/search-data/card/components/SearchContentItem.tsx
+++ webapp/src/search-data/card/components/SearchContentItem.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Box, Container, createStyles, Divider, Link, Theme, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { SimpleHotelDto } from '../../../core/simple-hotel.dto';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import clsx from 'clsx'
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -32,17 +31,11 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     secondaryColumn: {
       marginRight: theme.spacing(1),
-      width: theme.spacing(6),
+      width: theme.spacing(5),
       textAlign: 'right'
     },
-    noWidth: {
-      width: 'auto',
-      minWidth: theme.spacing(6),
+    lastColumn: {
       marginRight: theme.spacing(0),
-    },
-    hotelLink: {
-      marginLeft: theme.spacing(0.6),
-      height: theme.spacing(2.5),
     },
     hotelLinkIcon: {
       fontSize: theme.spacing(2.5),
@@ -57,6 +50,8 @@ interface SearchContentItemProps {
   readonly primaryKey: string;
   readonly secondaryValue: (h: SimpleHotelDto) => string | number;
   readonly secondaryKey: string;
+  readonly tertiaryValue: (h: SimpleHotelDto) => string | number;
+  readonly tertiaryKey: string;
 }
 
 export default function SearchContentItem({
@@ -66,6 +61,8 @@ export default function SearchContentItem({
                                             primaryKey,
                                             secondaryValue,
                                             secondaryKey,
+                                            tertiaryKey,
+                                            tertiaryValue,
                                           }: SearchContentItemProps) {
   const classes = useStyles()
   const empty =
@@ -83,10 +80,13 @@ export default function SearchContentItem({
           <Box className={classes.box}>
             <Typography className={classes.primaryColumn}
                         component="p">{title}:</Typography>
-            <Typography className={clsx(classes.secondaryColumn)}
+            <Typography className={clsx(classes.secondaryColumn, classes.tightLetters)}
+                        variant="body2"
+                        color='textSecondary'>{tertiaryKey}</Typography>
+            <Typography className={clsx(classes.secondaryColumn, classes.tightLetters)}
                         variant="body2"
                         color='textSecondary'>{secondaryKey}</Typography>
-            <Typography className={clsx(classes.secondaryColumn, classes.noWidth)}
+            <Typography className={clsx(classes.secondaryColumn, classes.tightLetters, classes.lastColumn)}
                         variant="body2"
                         color='textSecondary'>{primaryKey}</Typography>
           </Box>
@@ -94,28 +94,32 @@ export default function SearchContentItem({
           <Container className={classes.hotelsContainer}>
             {simpleHotels.map((hotel) => {
               return <Box key={hotel.hotelId} className={classes.box}>
-                <Typography className={clsx(classes.primaryColumn, classes.tightLetters)}
-                            noWrap
-                            variant="body2"
-                            color='textSecondary'>
-                  {hotel.name}
+                <Link className={classes.primaryColumn}
+                      color='primary'
+                      href={hotel.hotelLink}
+                      target="_blank">
+                  <Typography className={classes.tightLetters}
+                              noWrap
+                              variant="body2"
+                              color='textSecondary'>
+                    {hotel.name}
+                  </Typography>
+                </Link>
+                <Typography className={classes.secondaryColumn}
+                            variant='body2'
+                            color='secondary'>
+                  {tertiaryValue(hotel)}
                 </Typography>
                 <Typography className={classes.secondaryColumn}
                             variant='body2'
                             color='secondary'>
                   {secondaryValue(hotel)}
                 </Typography>
-                <Typography className={clsx(classes.secondaryColumn, classes.bold)}
+                <Typography className={clsx(classes.secondaryColumn, classes.bold, classes.lastColumn)}
                             variant='body2'
                             color='primary'>
                   {primaryValue(hotel)}
                 </Typography>
-                <Link className={classes.hotelLink}
-                      color='primary'
-                      href={hotel.hotelLink}
-                      target="_blank">
-                  <OpenInNewIcon className={classes.hotelLinkIcon}/>
-                </Link>
               </Box>
             })}
           </Container>
