f70850c83bb517e555cbcc4f7dde8ee95f422db0
piotrwestphal <piotr.westphal@gmail.com>
2021-04-01 23:58:18 +0200

    update init vm script | add filebeat config

--- cloud/opc/opc-init-vm.sh
+++ cloud/opc/opc-init-vm.sh
@@ -10,6 +10,10 @@ curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compo
 chmod +x /usr/bin/docker-compose
 yum install -y git
 
+# Install filebeat
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.12.0-x86_64.rpm
+yum localinstall -y filebeat-oss-7.12.0-x86_64.rpm
+
 # Security rules
 firewall-cmd --permanent --add-port=8080/tcp
 
@@ -22,3 +26,8 @@ mkdir /home/github-service/.ssh
 touch /home/github-service/.ssh/authorized_keys
 echo 'GITHUB_SERVICE_KEY_HERE' >> /home/github-service/.ssh/authorized_keys
 chown -R github-service /home/github-service
+
+# Filebeat config
+/bin/cp -f /home/github-service/app/cloud/opc/filebeat.yml /etc/filebeat/
+systemctl enable filebeat
+systemctl restart filebeat
