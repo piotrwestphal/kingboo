b2292d1169f6eabddfa0754470a356a04e9af96c
piotrwestphal <piotr.westphal@gmail.com>
2021-05-18 14:48:45 +0200

    add todo

--- backend/apps/user-service/src/app/search-data.controller.ts
+++ backend/apps/user-service/src/app/search-data.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common'
 import { SearchDataService } from './search-data/search-data.service'
 import { SearchDataPayload } from './search-data/search-data.payload'
 
+/*
+  TODO: proxy for search-queue-manager
+  - https://github.com/nestjs/nest/issues/2357
+  - https://stackoverflow.com/questions/58608556/how-to-proxy-an-endpoint-using-google-app-engine
+ */
 @Controller('api/v1/search-data')
 export class SearchDataController {
 
