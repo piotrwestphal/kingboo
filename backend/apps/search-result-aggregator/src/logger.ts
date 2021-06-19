e5fb710c1ba60d2ed1166755724d42801ccac444
piotrwestphal <piotr.westphal@gmail.com>
2021-06-19 17:08:59 +0200

    update log label

--- backend/apps/search-result-aggregator/src/logger.ts
+++ backend/apps/search-result-aggregator/src/logger.ts
@@ -1,7 +1,7 @@
 import { createLogger } from '@kb/logger'
 
 export const logger = createLogger({
-  appLabel: process.env.APP_LABEL || 'THA',
+  appLabel: process.env.APP_LABEL || 'SRA',
   logLevel: process.env.LOG_LEVEL || 'debug',
   logOutputFolder: 'output',
 })
