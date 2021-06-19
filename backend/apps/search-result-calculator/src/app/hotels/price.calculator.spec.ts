b4d3d4f3d22483c45d3b1b3f54830b5c9b5d1fe7
piotrwestphal <piotr.westphal@gmail.com>
2021-06-19 11:38:27 +0200

    add unit test for price calculation

--- backend/apps/search-result-calculator/src/app/hotels/price.calculator.spec.ts
+++ backend/apps/search-result-calculator/src/app/hotels/price.calculator.spec.ts
@@ -33,7 +33,7 @@ describe('PriceCalculator', () => {
     expect(priceRate).toBe(0)
   })
 
-  it('should calculate correct values', () => {
+  it('should calculate correct values [1]', () => {
     // given
     const currentPrice = 100
     const priceChanges: PriceChange[] = [
@@ -74,5 +74,54 @@ describe('PriceCalculator', () => {
     expect(maxPriceDiff).toBe(100)
     expect(minPrice).toBe(100)
     expect(priceRate).toBe(50)
+  })
+
+  it('should calculate correct values [2]', () => {
+    // given
+    const currentPrice = 100
+    const priceChanges: PriceChange[] = [
+      {
+        price: null,
+        room: null,
+        occurrenceCount: 5,
+        changedAt: null,
+      },
+      {
+        price: 100,
+        room: 'simple',
+        occurrenceCount: 2,
+        changedAt: null,
+      },
+      {
+        price: 150,
+        room: 'premium',
+        occurrenceCount: 3,
+        changedAt: null,
+      },
+      {
+        price: null,
+        room: null,
+        occurrenceCount: 1,
+        changedAt: null,
+      },
+    ]
+
+    // when
+    const {
+      avgPrice,
+      avgPriceDiff,
+      maxPrice,
+      maxPriceDiff,
+      minPrice,
+      priceRate
+    } = priceCalculator.calculate(currentPrice, priceChanges)
+
+    // then
+    expect(avgPrice).toBe(125)
+    expect(avgPriceDiff).toBe(25)
+    expect(maxPrice).toBe(150)
+    expect(maxPriceDiff).toBe(50)
+    expect(minPrice).toBe(100)
+    expect(priceRate).toBe(33.33)
   })
 });
