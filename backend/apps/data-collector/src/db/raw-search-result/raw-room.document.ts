c957c2530fbbdf7397f9dab15eb71d817fb1aacb
piotrwestphal <piotr.westphal@gmail.com>
2021-06-19 11:34:22 +0200

    update type

--- backend/apps/data-collector/src/db/raw-search-result/raw-room.document.ts
+++ backend/apps/data-collector/src/db/raw-search-result/raw-room.document.ts
@@ -1,6 +1,6 @@
 export interface RawRoomDocument {
   readonly shortDescription: string
-  readonly longDescription: string
+  readonly longDescription: string | null
   readonly personCount: string
   readonly beds: string
   readonly bonuses: string[] | null
