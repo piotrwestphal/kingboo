08e395406740d0f27bb3f35aaff9fb73035c7e01
piotrwestphal <piotr.westphal@gmail.com>
2021-06-27 22:01:46 +0200

    improve logging

--- backend/libs/logger/src/logger.factory.ts
+++ backend/libs/logger/src/logger.factory.ts
@@ -1,20 +1,22 @@
-import { transports } from 'winston';
-import { CommonLoggerService } from './common-logger.service';
-import { LoggerConfig } from './logger.config';
-import { FileTransportInstance } from 'winston/lib/winston/transports'
+import * as Winston from 'winston'
+import { CommonLoggerService } from './common-logger.service'
+import { LoggerConfig } from './logger.config'
+import * as DailyRotateFile from 'winston-daily-rotate-file'
 
 export const createLogger = ({
                                logLevel,
                                logOutputFolder,
                              }: LoggerConfig) => {
-  const additionalTransports: FileTransportInstance[] = [];
-  additionalTransports.push(new transports.File({
+  const additionalTransports: Winston.transport[] = []
+  additionalTransports.push(new DailyRotateFile({
     dirname: `${logOutputFolder}/logs`,
-    filename: 'output.log',
-    level: 'debug',
-  }));
+    filename: '%DATE%.log',
+    level: logLevel,
+    datePattern: 'YYYY-MM-DD',
+    maxFiles: '21d',
+  }))
   return new CommonLoggerService({
     logLevel,
     additionalTransports,
-  });
-};
+  })
+}
